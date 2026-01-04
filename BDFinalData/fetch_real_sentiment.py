9f006b33995f4b33f66b86aa2304b4962d0d5fc5
hchauhan39-FH <chauhanharman547@gmail.com>
2026-01-04 23:21:00 +0100

    API Key Update

--- BDFinalData/fetch_real_sentiment.py
+++ BDFinalData/fetch_real_sentiment.py
@@ -110,7 +110,7 @@ class EnhancedSentimentFetcher:
             # CryptoPanic free public API
             url = "https://cryptopanic.com/api/developer/v2/posts/"
             params = {
-                'auth_token': 'f72c9f31848a598c6386827e2f4125d9ad74df4b',
+                'auth_token': 'cef669b5f72036d4339b4c458b828eaead422efc',
                 'public': 'true'
             }
             
